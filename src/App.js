9eed84af58526ca58d646d9c6e977d54cd1e4ad2
thyagorafaell <thyagorafael012@gmail.com>
2019-06-20 12:22:54 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import H1 from './atoms/Headers/H1';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <H1>Título</H1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
